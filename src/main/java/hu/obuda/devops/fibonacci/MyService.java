a49c5e7441b5380a27c5c4179eeca5c9f9f3c619
Kováts Máté <kovats.mate19@gmail.com>
2022-09-19 12:30:25 +0200

    created service

---
+++ src/main/java/hu/obuda/devops/fibonacci/MyService.java
@@ -0,0 +1,27 @@
+package hu.obuda.devops.fibonacci;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyService {
+    
+    public ArrayList<Integer> GetFibonaccis(int n)
+    {
+        ArrayList<Integer> returnValue = new ArrayList<>();
+
+        for(int i=1; i<n; i++){
+            returnValue.add(fib(i));
+        }
+
+        return returnValue;
+    }
+
+    private int fib(int n)
+    {
+        if (n <= 1)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+}
